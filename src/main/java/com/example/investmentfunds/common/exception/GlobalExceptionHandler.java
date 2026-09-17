80430ea4dc7493570126d57cc63f809f3667defc
Juan Francisco <juanfranciscofernandezherreros@gmail.com>
2026-09-17 22:23:57 +0200

    test: add Cucumber API scenarios

--- src/main/java/com/example/investmentfunds/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/investmentfunds/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,21 @@
 package com.example.investmentfunds.common.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidation(ConstraintViolationException exception) {
+        log.warn("[FUND] - ACTION: validation failed: {}", exception.getMessage());
+        return new ErrorResponse("VALIDATION_ERROR", exception.getMessage());
+    }
+
     @ExceptionHandler(AppException.class)
     public ResponseEntity<ErrorResponse> handle(AppException e) {
         log.warn("[FUND] - ACTION: request failed: error: {}", e.getError().name());
---
+++ src/test/java/com/example/investmentfunds/cucumber/CucumberSuiteTest.java
@@ -0,0 +1,16 @@
+package com.example.investmentfunds.cucumber;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.investmentfunds.cucumber")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class CucumberSuiteTest {}
